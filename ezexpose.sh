9a17813e7ff9740ac0cf19cada730e78e03bec01
ebonynon <sithumsandeepap@gmail.com>
2020-10-10 07:20:28 +0530

    script ⛳

--- ezexpose.sh
+++ ezexpose.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+printf "\n  Welcome To EzExpose, Expose your deployment with EzExpose ezly... \n\n"
+
+read -p 'Deployment : ' deployment
+read -p 'Type: ' type
+read -p 'ServiceName: ' servicename
+
+printf "\n"
+
+kubectl expose deployment $deployment --type=$type --name=$servicename
